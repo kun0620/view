25c30611aa11c2ea7dc9cd075498d31360b1443d
kun0620 <41094738+kun0620@users.noreply.github.com>
2018-09-03 11:24:53 +0700

    แก้

--- index.php
+++ index.php
@@ -1,152 +1,97 @@
-<?php
-require "connect.php";
-$meSql = "SELECT * FROM pttes2 ";
-$meQuery = mysqli_query($con,$meSql);
- ?>
-<html lang="en">
+<html>
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>PTTES View Download</title>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+       <title>PTTES</title>
+		<STYLE TYPE="TEXT/CSS"><!--
+A:link	{
+	text-decoration:none;
+	color:#3366cc;
+	}
+A:hover {
+	color:#33ff33;
+	}
+A:visited	{
+text-decoration:none;
+}
+//--></STYLE>
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-<script src="js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-
-    <title>Edit Data</title>
-  </head>
-
+</head>
 <body>
-  <nav class="navbar fixed-top navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">DATA</a>
-  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Add DATA</button>
-  </nav>
+<body bgcolor="#FFFFFF" >
+   
+	<table width="100%" >
+	<tr>
+	<td bgcolor=blue><font color="#E6E6E6"size="30"><center>View Download</center></font></td>
+	</tr>
+	</table>
+	<br><br>
 
-  <form action="new.php" method="post">
-  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Add DATA</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <div class="form-row">
-<div class="form-group col-md-6">
-<label for="inputEmail4">tomerID</label>
-<input type="number" class="form-control" id="1" name="1" placeholder="tomerID">
-</div>
-<div class="form-group col-md-6">
-<label for="inputPassword4">Name(eng)</label>
-<input type="text" class="form-control" id="2" name="2"placeholder="Name(eng)">
-</div>
-</div>
-<div class="form-row">
-<div class="form-group col-md-5">
-<label for="inputEmail4">Name(Thai)</label>
-<input type="text" class="form-control" id="3" name="3" placeholder="Name(Thai)">
-</div>
-<div class="form-group col-md-3">
-  <label for="inputState">Dep</label>
-        <select id="5" name="5" class="form-control">
-          <option selected>PSD</option>
-          <option>KE</option>
-          <option>SHE</option>
-          <option>RAI</option>
-          <option>SCP</option>
-          <option>BD</option>
-          <option>HR</option>
-        </select>
-</div>
-<div class="form-group col-md-4">
-<label for="inputPassword4">Tel1</label>
-<input type="text" class="form-control" id="7" name="7" placeholder="Tel1">
-</div>
-</div>
-<div class="form-row">
-<div class="form-group col-md-4">
-<label for="inputEmail4">Tel2</label>
-<input type="text" class="form-control" id="8"  name="8" placeholder="Tel2">
-</div>
-<div class="form-group col-md-4">
-<label for="inputPassword4">O1</label>
-<input type="text" class="form-control" id="9" name="9" placeholder="O1">
-</div>
-<div class="form-group col-md-4">
-<label for="inputPassword4">Email</label>
-<input type="email" class="form-control" id="10" name="10" placeholder="Email">
-</div>
-</div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary">ADD</button>
-      </div>
-    </div>
-  </div>
-</div>
+<?php
+	ini_set('display_errors', 1);
+	error_reporting(~0);
+
+	$strKeyword = null;
+
+	if(isset($_POST["txtKeyword"]))
+	{
+		$strKeyword = $_POST["txtKeyword"];
+	}
+?>
+<form name="frmSearch" method="post" action="<?php echo $_SERVER['SCRIPT_NAME'];?>">
+  <table width="100%" >
+    <tr>
+      <th align ="left" >
+      <td><input style ="height:70px; background:#ffffff; width:95%; font-size:20pt;" placeholder="Search : Project Name" name="txtKeyword" type="text" id="txtKeyword" value="<?php echo $strKeyword;?>"></td>
+      <td><input Style ="align:center; height:100px; width:100px;" type="image" src="search.jpg" alt="submit" value="Search"></th></td>
+    </tr>
+  </table>
 </form>
-  <br>
-  <br>
-  <br>
-  <table class="table table-striped">
-                  <thead>
-                      <tr>
-                          <th scope="col">tomerID</th>
-                          <th scope="col">Name</th>
-                          <th scope="col">Name2</th>
 
-                          <th scope="col">Dep</th>
+<?php
 
-                          <th scope="col">Tel1</th>
-                          <th scope="col">Tel2</th>
-                          <th scope="col">O1</th>
-                          <th scope="col">Email</th>
-                          <th scope="col">#</th>
-                      </tr>
-                  </thead>
-                  <tbody>
-                      <?php
-                      while ($meResult = mysqli_fetch_assoc($meQuery))
-                      {
-                          ?>
-                          <tr >
+   $serverName = "localhost";
+   $userName = "root";
+   $userPassword = "rklg;biNf";
+   $dbName = "mydatabase";
 
-                              <th scope="row"><?php echo $meResult['tomerID']; ?></td>
-                              <td><?php echo $meResult['Name']; ?></td>
-                              <td><?php echo $meResult['Name2']; ?></td>
+   $conn = mysqli_connect($serverName,$userName,$userPassword,$dbName);
+	
+   $sql = "SELECT * FROM register WHERE Project LIKE '%".$strKeyword."%' ";
 
-                              <td><?php echo $meResult['Dep']; ?></td>
+   $query = mysqli_query($conn,$sql);
+   $row= mysqli_num_rows($query);
 
-                              <td><?php echo $meResult['Tel1']; ?></td>
-                              <td><?php echo $meResult['Tel2']; ?></td>
-                              <td><?php echo $meResult['O1']; ?></td>
-                              <td><?php echo $meResult['Email']; ?></td>
-                              <td><button class="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">เพิ่มเติม</button>
-                                <div class="dropdown-menu">
+?>
+<table width="100%" border="0">
+  
+<?php
+$Total = 1;
+error_reporting(~E_NOTICE);
+while($result=mysqli_fetch_array($query,MYSQLI_ASSOC))
+{
+	
+	
+?>
 
-                                <a class="dropdown-item"href="edit.php?editId=<?php echo $meResult['tomerID']; ?>" role="button">
+  <tr>
+    <td><font size = "5" color ="3300FF" ><?php echo $result["Project"];?></font></td>
+    <td><font size = "5" color ="3300FF" ><?php echo $result["Date-Time"];?></font></td>
+	<td><font size = "5" color ="3300FF" ><?php echo $result["email"];?></font></td>
+	</tr>
+	
+	
+	
+<?php
 
-                                    แก้ไข</a>
-                                  <a class="dropdown-item" href="delete.php?Id=<?php echo $meResult['tomerID']; ?>" role="button">
+}
+?>
+</table>
 
-                                      ลบ</a>
-</div>
-                              </td>
-
-                          </tr>
-                          <?php
-                      }
-                      ?>
-                        <form method="post" action="new.php?Id=<?php echo $meResult['tomerID']; ?>">
-
-                          </form>
-                  </tbody>
-              </table>
-              <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-                 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-                 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+<h1><?php echo "Totol Download = ".$row; ?></h1>
+<?php
+	
+mysqli_close($conn);
+?>
 </body>
 </html>
